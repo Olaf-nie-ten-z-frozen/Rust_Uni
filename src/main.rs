de47b19bf7056b87ccd20c96a1a7d20bdee26568
Olaf <Tkaczyk68@gmai.com>
2023-03-07 11:21:30 +0100

    Rust Basics Lesson

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn szlaczek(){
+	let a = 0;
+	let b = 4;
+	if(a == 0 || b == 0){
+	println!("Udalo sie");
+	} 
+	else{
+	println!("Jest gdzies nieodpowiednia liczba...");
+}
+}
+fn main() {
+    szlaczek()
+	
+}
